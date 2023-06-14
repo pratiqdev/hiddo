3fa9dfac649f775a4559538a33d7dd793c7c706a
pratiqdev <pratiqdev@gmail.com>
2023-06-14 11:58:00 -0400

    Updating refresh method

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,14 @@ import * as path from 'path';
 import * as glob from 'glob';
 import { minimatch } from 'minimatch';
 
-const CONFIG_FILE_PATH = './hiddo.json';
+const CONFIG_FILE_PATH = 'hiddo.json';
 let cwd = null;
 
 
 const log = console.log;
 
 type State = {
+	rootPath: string | null;
 	zones: Record<string, string[]>;
 	zone: null | string;
 	index: number;
@@ -27,10 +28,11 @@ const state:State = {
 	enabled: false,
 	defaults: {},
 	hasDefaults: false,
+	rootPath: null
 };
 
 
-//&																																											
+//&																														
 const setExclusionRules = (rules:Record<string, boolean>) => {
 	vscode.workspace.getConfiguration().update('files.exclude', rules, vscode.ConfigurationTarget.Workspace);
 };
@@ -38,83 +40,100 @@ const setExclusionRules = (rules:Record<string, boolean>) => {
 
 
 
-//&																																											
+//&																														
 function generateAndSetExcludeRules(): void {
+	try{
 
-	if(!state.zone || !state.hasDefaults || !Object.keys(state.zones).length){
-		log("Cannot generate exclude rules:", state);
-		return;
+		if(!state.zone || !state.hasDefaults || !Object.keys(state.zones).length){
+			log("Cannot generate exclude rules:", state);
+			return;
+		}
+
+		// Get the glob patterns for the specified zone
+		let globPatterns: string[] = state.zones[state.zone];
+		// Initialize exclude rules
+		let excludeRules: Record<string, boolean> = {};
+
+		// Get a list of all files and directories in workspace
+		let allFilesAndDirs = glob.sync('**', { 
+			cwd: state.rootPath!, 
+			dot: true,
+			mark: true // add a trailing / to directories
+		});
+
+
+		// Exclude files/directories that do not match any glob pattern in the specified zone
+		allFilesAndDirs.forEach((fileOrDir:string) => {
+			let isExcluded = !globPatterns.some(pattern => minimatch(fileOrDir, pattern));
+			excludeRules[fileOrDir] = isExcluded;
+		});
+
+		setExclusionRules(excludeRules);
+	}catch(err){
+		log("Error generating exclusion rules:");
+		log(err);
 	}
-
-    // Get the glob patterns for the specified zone
-    let globPatterns: string[] = state.zones[state.zone];
-	// Initialize exclude rules
-	let excludeRules: Record<string, boolean> = {};
-
-	if(!vscode?.workspace?.workspaceFolders || !vscode.workspace.workspaceFolders[0]?.uri?.fsPath){
-		log('No open workspaces or directories...');
-		return;
-	}
-
-    // Get a list of all files and directories in workspace
-	let rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-    let allFilesAndDirs = glob.sync('**', { 
-        cwd: rootPath, 
-        dot: true,
-        mark: true // add a trailing / to directories
-    });
-
-
-    // Exclude files/directories that do not match any glob pattern in the specified zone
-    allFilesAndDirs.forEach((fileOrDir:string) => {
-        let isExcluded = !globPatterns.some(pattern => minimatch(fileOrDir, pattern));
-        excludeRules[fileOrDir] = isExcluded;
-    });
-
-	setExclusionRules(excludeRules);
 
 }
 
 
 
-//&																																											
+//&																														
 const refresh = () => {
+	try{
 
-	// refresh the config file
-	let configFile = JSON.parse(fs.readFileSync(CONFIG_FILE_PATH, 'utf8'));
-
-	if(!configFile || !Object.keys(configFile).length){
-		log("No config file found...");
-		return;
-	}else{
-		state.zones = configFile;
-	}
-
-	if(!state.hasDefaults){
-		log("No defaults set. Set defaults with 'hiddo.defaults' to save the current exclusion rules");
-		return;
-	}
-
-	if(!Object.entries(state.zones).length){
-		log("No zones defined. Check your 'hiddo.json' config file.");
-		return;
-	}
-
-	if(!state.zone){
-		let keys = Object.keys(state.zones);
-		if(keys.length){
-			log("Setting active zone to first provided zone.");
-			state.zone = keys[0];
-		}else{
-			log("No active zone and no zones available... Check you 'hiddo.json' config.");
+		// get the root path
+		if(!vscode?.workspace?.workspaceFolders || !vscode.workspace.workspaceFolders[0]?.uri?.fsPath){
+			log('No open workspaces or directories...');
 			return;
 		}
-	}
+		state.rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		log("Found root path:", state.rootPath);
 
-	if(state.enabled){
-		generateAndSetExcludeRules();
-	}else{
-		setExclusionRules(state.defaults);
+		let configPath = path.join(state.rootPath, CONFIG_FILE_PATH);
+		log("Using config path:", configPath);
+
+		// refresh the config file
+		return;
+		let configFile = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+		if(!configFile || !Object.keys(configFile).length){
+			log("No config file found...");
+			return;
+		}else{
+			state.zones = configFile;
+		}
+
+		if(!state.hasDefaults){
+			log("No defaults set. Set defaults with 'hiddo.defaults' to save the current exclusion rules");
+			return;
+		}
+
+		if(!Object.entries(state.zones).length){
+			log("No zones defined. Check your 'hiddo.json' config file.");
+			return;
+		}
+
+		if(!state.zone){
+			let keys = Object.keys(state.zones);
+			if(keys.length){
+				log("Setting active zone to first provided zone.");
+				state.zone = keys[0];
+			}else{
+				log("No active zone and no zones available... Check you 'hiddo.json' config.");
+				return;
+			}
+		}
+
+		if(state.enabled){
+			generateAndSetExcludeRules();
+		}else{
+			setExclusionRules(state.defaults);
+		}
+
+	}catch(err){
+		log("Error refreshing hiddo:");
+		log(err);
 	}
 
 };
